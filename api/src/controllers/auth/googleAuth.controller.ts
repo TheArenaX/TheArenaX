37e7d6d5a2552db2484d836f78635e0e045f88f8
Upayan M <upayanm3@gmail.com>
2025-06-21 02:02:41 +0530

    refactor: remove unused authentication strategies and related code for cleaner structure

--- api/src/controllers/auth/googleAuth.controller.ts
+++
@@ -1,7 +0,0 @@
-import passport from "passport";
-
-const googleAuth = passport.authenticate("google", {
-  scope: ["email", "profile"],
-});
-
-export default googleAuth;
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,3 @@
-import env from "./config/env";
-import mongoose from "mongoose";
 import api from "./api";
 import { log } from "./utils/logging/logger";
 
@@ -7,9 +5,6 @@ const PORT = 5000;
 
 async function startServer() {
   try {
-    await mongoose.connect(env.MONGO_URI!);
-    log({ type: "success", message: "MongoDB connected" });
-
     api.listen(PORT, () => {
       log({ type: "success", message: `Server running on port ${PORT}` });
       log({ type: "info", message: `API URL: http://localhost:${PORT}` });
