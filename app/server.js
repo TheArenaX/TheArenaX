c921c0f68b0372822f5ab33f6da3bcd44e637d0b
Pranay-kumaar <pranay10a35.hts21@gmail.com>
2025-07-02 02:54:39 +0530

    nvm

--- app/server.js
+++ app/server.js
@@ -50,7 +50,7 @@ app.use(async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
